1ef57a004035004e50b3092d7d9f0bc33987881f
luigi <luigi_lipreri@hotmail.com>
2021-08-26 10:22:29 -0300

    agregado funcionar como echo server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-const io = require("socket.io-client");
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000', {reconnect: true});
+//let socket = io.connect("http://54.144.208.212:3000/");
+let mensaje = '';
+let userName = 'Luigo';
 
-let games = io.connect("http://localhost:3000/games");
-
-games.on("welcome", (msg) => {
-    console.log("recibido ", msg);
+readline.on('line', (line) => {
+    mensaje = line;
+    socket.emit('CH01', userName, mensaje);
 })
+
+
+socket.on('connect', function () {
+    data = {name: userName, userId: socket.id};
+    socket.emit('login',data);
+});
+
+socket.on('serverWellcome', function (data) {
+    console.log(data);
+});
+
+socket.on('echo', function (data) {
+    console.log(data);
+});
--- src/server.js
+++ src/server.js
@@ -1,21 +1,52 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-const http = require("http").createServer();
-const io = require("socket.io")(http);
-/*
-io.on("connection", (socket) => {
-    console.log("este es el IO ",io);
-    socket.emit("welcome", "Bienvenidossss chamigossss");
-});
-*/
+//server.js
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// probar despues private msg
-io.of("/test")
-.on("connection", (socket) => {
-    socket.emit("welcome", "Bienvenido, te conectaste a la ruta del test!!!");
+const logo = " \
+/ ____/___  ____ ___  ____  ____  ________  _____ \
+/ /   / __ \/ __ `__ \/ __ \/ __ \/ ___/ _ \/ ___/ \
+/ /___/ /_/ / / / / / / /_/ / /_/ (__  )  __/ / \
+\____/\____/_/ /_/ /_/ .___/\____/____/\___/_/ \
+                   /_/ \
+";
+
+
+
+io.on('connection', function (socket){
+   console.log('se conecto un nuevo usuario');
+   
+  socket.on('login', function(data){
+    console.log('a user ' + data.userId + ' connected');
+    // saving userId to object with socket ID
+    // users[socket.id] = data.userId;
+  });
+  socket.on("disconnect", (reason) => {
+    console.log(`se desconecto el cliente ${socket.id} debido a ${reason}`);
+  });
+
+  socket.on('CH01', function (from, msg) {
+    console.log(from, ' saying: ', msg);
+    
+    // sending to all clients except sender
+    socket.broadcast.emit('echo', msg);
+    // io.emit('echo', msg);
+   
+  });
+
+
+   // sending to individual socketid (private message)
+   // socket.to(<socketid>).emit('hey', 'I just met you');
+  
+   io.emit('serverWellcome', {
+    id: 1,
+    text: 'hola Bienvenido desde el servidor',
+    author: 'The Server'
+  } );
+
 });
 
-http.listen(PORT, () =>{
-    console.log("Server is listening on localhost:3000");
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
 });
